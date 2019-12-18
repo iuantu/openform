a8e0eb36938ee6d95c218a74385d7cf9458bb54c
CJ <cj@iuantu.com>
2019-12-18 17:22:25 +0800

    Swagger documentation and domain model

---
+++ app/views/__init__.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from app import appbuilder
+from app.views.control_panel import *
+from app.views.form import *
+from app.views.user import *
+
+@appbuilder.app.errorhandler(404)
+def page_not_found(e):
+    return (
+        render_template(
+            "404.html", base_template=appbuilder.base_template, appbuilder=appbuilder
+        ),
+        404,
+    )
