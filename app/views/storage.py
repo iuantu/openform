54a5195e5dbf8869293c6caff3ce6de85e7e305d
CJ <cj@iuantu.com>
2020-01-05 00:04:42 +0800

    完成接口，补充key参数。

--- app/views/storage.py
+++ app/views/storage.py
@@ -1,6 +1,6 @@
 from app import appbuilder
 from flask_appbuilder.api import BaseApi, expose
-from flask import jsonify
+from flask import jsonify, request
 import os
 import json
 import hashlib
@@ -12,42 +12,56 @@ from optparse import OptionParser
 
 OSS_ACCESS_KEY_SECRET = os.getenv("OSS_ACCESS_KEY_SECRET")
 
-def convert_base64(input):
-    return base64.b64encode(input.encode("utf-8"))
 
-def get_sign_policy(key, policy):
-    hash_object = hmac.new(key.encode(), policy, hashlib.sha1)
-    hash_value = hash_object.digest()
-    return base64.b64encode(hash_value).decode("utf-8")
 
-class StorageAPI(BaseApi):
+class Signature:
+    def __init__(self, key, access_key_secret):
+        self.access_key_secret = access_key_secret
+        self.key = key
+        self._calculate()
+        
 
-    resource_name = 'storage'
+    def asdict(self):
+        return {
+            "key": self.key,
+            "policy": self.policy_base64.decode(),
+            "signature": self.signature,
+        }
 
-    @expose("signature")
-    def signature(self):
-        # TODO: generate key by file name
+    def _calculate(self):
         now = datetime.datetime.now()
-        key = "usr/%s/%d/%f" % (now.strftime("%Y-%m-%d"), 1, random.random())
+        self.key = "usr/%s/%f-%s" % (now.strftime("%Y-%m-%d"), random.random(), self.key)
         expiration = now + datetime.timedelta(days=7)
         policy = {
             "expiration": expiration.strftime("%Y-%m-%dT%H:%M:%SZ"),
             "conditions": [
                 {
                     "bucket": os.getenv("OSS_BUCKET"),
-                #     "key": key
+                    "key": self.key,
                 },
                 ["content-length-range", 0, 1048576],
             ]
         }
         policy_string = json.dumps(policy)
-        base64policy = convert_base64(policy_string)
-        signature = get_sign_policy(OSS_ACCESS_KEY_SECRET, base64policy)
+        self.policy_base64 = self._convert_base64(policy_string)
+        self.signature = self._get_sign_policy(self.policy_base64)
 
-        return jsonify({
-            "key": key,
-            "policy": base64policy.decode("utf-8"),
-            "signature": signature
-        })
+    def _convert_base64(self, input):
+        return base64.b64encode(input.encode("utf-8"))
+
+    def _get_sign_policy(self, policy):
+        hash_object = hmac.new(self.access_key_secret.encode(), policy, hashlib.sha1)
+        hash_value = hash_object.digest()
+        return base64.b64encode(hash_value).decode("utf-8")
+
+class StorageAPI(BaseApi):
+
+    resource_name = 'storage'
+
+    @expose("signature", methods=["POST"])
+    def signature(self):
+        keys = request.form.getlist("key[]")
+        signatures = [(Signature(key, OSS_ACCESS_KEY_SECRET)).asdict() for key in keys]
+        return jsonify(signatures)
 
 appbuilder.add_api(StorageAPI)
