0cecc31d521e2dba68129937c983c04e955c6b05
yanghao <yanghao@supersets.in>
2020-01-16 23:12:31 +0800

    (fixed) baseURL error

--- openform/src/functions.js
+++ openform/src/functions.js
@@ -1,9 +1,9 @@
 export let baseURL;
 
 if (process.env.NODE_ENV === 'development') {
-    baseURL = "http://localhost:5000";
+    baseURL = `${document.location.protocol}//${document.location.hostname}:5000`;
 } else {
-    baseURL = "";
+    baseURL = `${document.location.protocol}//${document.location.host}`;
 }
 
 class UIBase {
