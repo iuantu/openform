60023264a9251a49be796a8a80bbc74a416c2b21
cenxiaoling <2357864954@qq.com>
2020-02-15 22:11:35 +0800

    add preview

---
+++ openform/src/components/formPreviewComponent/components/multiSelect/multiSelect.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="form-detail-content" :style="{width: formItm.width}">
+        <div class="form-title" :style="{textAlign: formItm.alignType}">
+            {{formItm.name}}
+            <span class="form-require" v-if="formItm.isRequired">*</span>
+        </div>
+        <div class="form-subtitle" :style="{textAlign: formItm.alignType}">{{formItm.subTitle}}</div>
+        <div class="select-opts">
+            <el-checkbox-group v-model="select">
+                <el-checkbox v-for="(checkItm, checkIndex) in formItm.options" :key="checkIndex + '_checkbox'" :label="checkItm.label">
+                {{checkItm.value}}
+                </el-checkbox>
+            </el-checkbox-group>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            select: []
+        }
+    },
+  props: ['formIndex', 'formItm']
+}
+</script>
+<style lang="scss">
+
+</style>
